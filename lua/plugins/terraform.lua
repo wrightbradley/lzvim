c36e3460284ddf31c35294b0d094ac5e1c92a259
wrightbradley <bradley.wright@tackle.io>
2023-02-09 10:58:40 -0500

    Added vim-terraform plugin

--- lua/plugins/terraform.lua
+++ lua/plugins/terraform.lua
@@ -37,4 +37,11 @@ return {
       },
     },
   },
+  {
+    "hashivim/vim-terraform",
+    config = function()
+      vim.cmd([[let g:terraform_fmt_on_save=1]])
+      vim.cmd([[let g:terraform_align=1]])
+    end,
+  },
 }
