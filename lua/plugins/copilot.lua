15a27c4e8af66bd2c3cf488a8af407e5e26a0fdc
wrightbradley <bradley.wright.tech@gmail.com>
2023-02-13 10:38:46 -0500

    document GH Pilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,8 +2,21 @@ return {
   {
     "github/copilot.vim",
     config = function()
-      vim.g.copilot_no_tab_map = true
       vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      vim.g.copilot_no_tab_map = true
+      -- Other Maps
+      -- *copilot-i_CTRL-]*
+      -- <C-]>                   Dismiss the current suggestion.
+      -- <Plug>(copilot-dismiss)
+      -- *copilot-i_ALT-]*
+      -- <M-]>                   Cycle to the next suggestion, if one is available.
+      -- <Plug>(copilot-next)
+      -- *copilot-i_ALT-[*
+      -- <M-[>                   Cycle to the previous suggestion.
+      -- <Plug>(copilot-previous)
+      -- *copilot-i_ALT-\*
+      -- <M-\>                   Explicitly request a suggestion, even if Copilot
+      -- <Plug>(copilot-suggest) is disabled.
     end,
   },
 }
