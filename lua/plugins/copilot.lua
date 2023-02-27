2ef6bfca4745741d3fe605b8e65f199cb6f0a50a
wrightbradley <bradley.wright.tech@gmail.com>
2023-02-26 20:43:48 -0500

    Potential indent fix

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -68,20 +68,20 @@ return {
     opts = function(_, opts)
       local cmp = require("cmp")
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot", group_index = 2 } }))
-      -- mapping = cmp.mapping.preset.insert({
-      --   ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-      --   ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-      --   ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      --   ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      --   ["<C-Space>"] = cmp.mapping.complete(),
-      --   ["<C-e>"] = cmp.mapping.abort(),
-      --   -- ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      --   ["<CR>"] = cmp.mapping.confirm({
-      --     -- this is the important line
-      --     behavior = cmp.ConfirmBehavior.Replace,
-      --     select = false,
-      --   }),
-      -- })
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        -- ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<CR>"] = cmp.mapping.confirm({
+          -- this is the important line
+          select = true,
+          behavior = cmp.ConfirmBehavior.Replace,
+        }),
+      })
       -- formatters = {
       --   insert_text = require("copilot_cmp.format").remove_existing,
       -- }
