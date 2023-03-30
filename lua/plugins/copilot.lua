1bdd416d8d45e86866ffda8e3ac81a67344feb23
wrightbradley <bradley.wright.tech@gmail.com>
2023-03-30 08:36:37 -0400

    Configured GH CoPilot using LazyVim configuration

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,9 @@
 return {
+
+  --- **************************************
+  --- This plugin is based on vimscript and not Lua. It is not under active development.
+  --  This is just here for reference.
+  --- **************************************
   -- https://www.reddit.com/r/neovim/comments/sk70rk/using_github_copilot_in_neovim_tab_map_has_been/
   -- {
   --   "github/copilot.vim",
@@ -42,49 +47,86 @@ return {
   -- <Plug>(copilot-suggest) is disabled.
   --   end,
   -- },
+
+  --- **************************************
+  --- These plugins have valid configuration.
+  --  These plugins work and operate correctly.
+  --  They are commented out for now in order to test
+  --  the configuration as defined from the LazyVim project.
+  --- **************************************
+  --   {
+  --     "zbirenbaum/copilot.lua",
+  --     cmd = "Copilot",
+  --     event = "InsertEnter",
+  --     config = function()
+  --       require("copilot").setup({
+  --         suggestion = { enabled = false },
+  --         panel = { enabled = false },
+  --       })
+  --     end,
+  --   },
+  --   {
+  --     "zbirenbaum/copilot-cmp",
+  --     dependencies = { "copilot.lua" },
+  --     config = function()
+  --       require("copilot_cmp").setup({
+  --         -- clear_after_cursor = true,
+  --         formatters = {
+  --           insert_text = require("copilot_cmp.format").remove_existing,
+  --         },
+  --       })
+  --     end,
+  --   },
+  --   {
+  --     "hrsh7th/nvim-cmp",
+  --     ---@param opts cmp.ConfigSchema
+  --     opts = function(_, opts)
+  --       local cmp = require("cmp")
+  --       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot", group_index = 2 } }))
+  --       opts.mapping = cmp.mapping.preset.insert({
+  --         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+  --         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+  --         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+  --         ["<C-f>"] = cmp.mapping.scroll_docs(4),
+  --         ["<C-Space>"] = cmp.mapping.complete(),
+  --         ["<C-e>"] = cmp.mapping.abort(),
+  --         -- ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  --         ["<CR>"] = cmp.mapping.confirm({
+  --           -- select = true,
+  --           select = false,
+  --           behavior = cmp.ConfirmBehavior.Replace,
+  --         }),
+  --       })
+  --     end,
+  --   },
+
+  --- **************************************
+  --- These plugins and config are from the docs
+  --  of the LazyVim project. I am currently testing this out.
+  --- **************************************
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      })
-    end,
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    },
   },
   {
     "zbirenbaum/copilot-cmp",
-    after = { "copilot.lua" },
-    config = function()
-      require("copilot_cmp").setup({
-        clear_after_cursor = true,
-      })
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot", group_index = 2 } }))
-      opts.mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        -- ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<CR>"] = cmp.mapping.confirm({
-          -- this is the important line
-          select = true,
-          behavior = cmp.ConfirmBehavior.Replace,
-        }),
-      })
-      -- formatters = {
-      --   insert_text = require("copilot_cmp.format").remove_existing,
-      -- }
+    dependencies = "copilot.lua",
+    opts = {},
+    config = function(_, opts)
+      local copilot_cmp = require("copilot_cmp")
+      copilot_cmp.setup(opts)
+      -- attach cmp source whenever copilot attaches
+      -- fixes lazy-loading issues with the copilot cmp source
+      require("lazyvim.util").on_attach(function(client)
+        if client.name == "copilot" then
+          copilot_cmp._on_insert_enter()
+        end
+      end)
     end,
   },
 }
