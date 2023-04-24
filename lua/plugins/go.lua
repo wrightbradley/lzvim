f17aa5ed60afb9b08857f24e8d71b9dc2034f75d
wrightbradley <bradley.wright.tech@gmail.com>
2023-04-24 09:51:30 -0400

    disable go format as it conflicted with other files

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,3 +1,6 @@
+-- if true then
+--   return {}
+-- end
 return {
   --- languages config ---
   -- golang
@@ -9,24 +12,18 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
       require("go").setup({
         goimport = "gopls", -- Use gopls for imports
         max_line_len = 120, -- Max line length for formatting
-        -- tag_transform = "snakecase", -- Use snake_case for tags
         verbose = false,
         lsp_cfg = {
-          capabilities = capabilities,
-          -- other setups
+          capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
         },
         lsp_gofumpt = true, -- Use gofumpt with LSP formatting
-        -- lsp_on_attach = false, -- Disable LSP on attach
-        -- dap_debug = false, -- Disable DAP debugger
       })
-      require("go.format").goimport()
+      -- require("go.format").goimport()
     end,
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
 }
