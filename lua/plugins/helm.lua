2303c6e078a36b7bb744778a9bbb840d9d3fac41
wrightbradley <bradley.wright.tech@gmail.com>
2023-07-19 08:10:36 -0400

    feat: move autocmds to autocmds file

--- lua/plugins/helm.lua
+++ lua/plugins/helm.lua
@@ -1,34 +1,3 @@
--- Syntax for Helm templates (Go templates inside YAML)
-
--- vim.api.nvim_create_autocmd("BufRead,BufNewFile", {
---   group = augroup,
---   pattern = "*/templates/*.yaml,*/templates/*.tpl,helmfile*.yaml,*/templates/*/*.yaml",
---   callback = function()
---     vim.bo.filetype = "helm"
---     -- vim.api.nvim_buf_del_var(0, "current_syntax")
---     -- vim.cmd([[syn include @yamlGoTextTmpl syntax/gotexttmpl.vim]])
---     -- vim.api.nvim_buf_set_var(0, "current_syntax", "yaml")
---     -- vim.cmd([[
---     --   syn region goTextTmpl start=/{{/ end=/}}/ contains=@gotplLiteral,gotplControl,gotplFunctions,gotplVariable,goTplIdentifier containedin=ALLBUT,goTextTmpl keepend
---     --   hi def link goTextTmpl PreProc
---     -- ]])
---   end,
--- })
-
-local function augroup(name)
-  return vim.api.nvim_create_augroup("helm_syntax" .. name, { clear = true })
-end
-
--- set helm filetype
-vim.api.nvim_create_autocmd("BufRead,BufNewFile", {
-  group = augroup("helm filetype"),
-  pattern = "*/templates/*.yaml,*/templates/*.tpl,helmfile*.yaml,*/templates/*/*.yaml",
-  callback = function()
-    vim.bo.filetype = "helm"
-    vim.bo.commentstring = "{{/* %s */}}"
-  end,
-})
-
 return {
   {
     "towolf/vim-helm",
