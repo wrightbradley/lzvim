f50a3125a0d6b55b0fa388353a8bc8236d7be527
wrightbradley <bradley.wright.tech@gmail.com>
2023-07-21 09:34:19 -0400

    feat: add twilight zen mode plugin

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -45,4 +45,12 @@ return {
       })
     end,
   },
+  {
+    "folke/twilight.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
