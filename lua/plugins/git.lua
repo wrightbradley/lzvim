1d960430754b2f4f6cf49501d221896f36ec89c8
wrightbradley <bradley.wright.tech@gmail.com>
2023-08-10 19:50:54 -0400

    feat: add support for Git Worktrees
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -18,4 +18,23 @@ return {
       require("octo").setup()
     end,
   },
+  {
+    "ThePrimeagen/git-worktree.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("git-worktree").setup()
+      require("telescope").load_extension("git_worktree")
+    end,
+    keys = {
+      { "<leader>gws", "<cmd>Telescope git_worktree git_worktrees<cr>", desc = "Switch git worktrees" },
+      {
+        "<leader>gwc",
+        "<cmd>Telescope git_worktree create_git_worktree<cr>",
+        desc = "Create and switch to new worktree",
+      },
+    },
+  },
 }
