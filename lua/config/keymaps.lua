60fb971e0b313604fa05e840a68c5a89c81e3947
wrightbradley <bradley.wright.tech@gmail.com>
2023-08-10 19:50:22 -0400

    feat: add snippets
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local ls = require("luasnip")
+
+vim.keymap.set({ "i", "s" }, "<C-I>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end, { silent = true })
