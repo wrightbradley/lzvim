efb912f9e3d6a7fc966107f03dcd3c9713de71f5
wrightbradley <bradley.wright.tech@gmail.com>
2023-11-02 06:55:20 -0400

    feat: use nvim-lint
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,7 +29,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.util.project" },
     -- import/override with your plugins
     { import = "plugins" },
