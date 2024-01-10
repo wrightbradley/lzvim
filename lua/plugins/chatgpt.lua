c5b14c2865ad48e9693dd472a09907a48248d204
wrightbradley <bradley.wright.tech@gmail.com>
2024-01-10 09:38:51 -0500

    feat: configure chatgpt.nvim to use ChatGPT-4
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -6,6 +6,23 @@ return {
       local home = vim.fn.expand("$HOME")
       require("chatgpt").setup({
         api_key_cmd = "gpg --decrypt " .. home .. "/chatgpt.key.gpg",
+        openai_params = {
+          model = "gpt-4-1106-preview",
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          max_tokens = 4096,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+        openai_edit_params = {
+          model = "gpt-4-1106-preview",
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
       })
     end,
     dependencies = {
