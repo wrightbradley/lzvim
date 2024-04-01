8f4ccc6056e6c14d40060c804f28fe43b004ef4b
wrightbradley <bradley.wright.tech@gmail.com>
2024-04-01 12:15:21 -0400

    feat: disable time from lualine
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/plugins/core/ui/lualine.lua
+++ lua/plugins/core/ui/lualine.lua
@@ -94,11 +94,11 @@ return {
             { "progress", separator = " ", padding = { left = 1, right = 0 } },
             { "location", padding = { left = 0, right = 1 } },
           },
-          lualine_z = {
-            function()
-              return " " .. os.date("%R")
-            end,
-          },
+          -- lualine_z = {
+          --   function()
+          --     return " " .. os.date("%R")
+          --   end,
+          -- },
         },
         extensions = { "neo-tree", "lazy" },
       }
