c50463b94d2604dbd90695b58e1fc0f70ca6f980
wrightbradley <bradley.wright.tech@gmail.com>
2024-04-15 07:19:40 -0400

    merge keymaps and options
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/config/keymaps.lua
+++ lua/wrightbradley/config/keymaps.lua
@@ -2,6 +2,9 @@
 --  See `:help map()`
 -- Keymaps are automatically loaded on the VeryLazy event
 
+-- This file is automatically loaded by config.init
+local Util = require("wrightbradley.util")
+
 -- local function map(mode, lhs, rhs, opts)
 --   local keys = require("lazy.core.handler").handlers.keys
 --   ---@cast keys LazyKeysHandler
@@ -132,6 +135,11 @@ map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
 map("n", "[q", vim.cmd.cprev, { desc = "Previous quickfix" })
 map("n", "]q", vim.cmd.cnext, { desc = "Next quickfix" })
 
+-- formatting
+map({ "n", "v" }, "<leader>cf", function()
+  Util.format({ force = true })
+end, { desc = "Format" })
+
 -- Diagnostic keymaps
 local diagnostic_goto = function(next, severity)
   local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
@@ -148,11 +156,40 @@ map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
 map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
 map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 
+-- stylua: ignore start
+
+-- toggle options
+map("n", "<leader>uf", function() Util.format.toggle() end, { desc = "Toggle auto format (global)" })
+map("n", "<leader>uF", function() Util.format.toggle(true) end, { desc = "Toggle auto format (buffer)" })
+map("n", "<leader>us", function() Util.toggle("spell") end, { desc = "Toggle Spelling" })
+map("n", "<leader>uw", function() Util.toggle("wrap") end, { desc = "Toggle Word Wrap" })
+map("n", "<leader>uL", function() Util.toggle("relativenumber") end, { desc = "Toggle Relative Line Numbers" })
+map("n", "<leader>ul", function() Util.toggle.number() end, { desc = "Toggle Line Numbers" })
+map("n", "<leader>ud", function() Util.toggle.diagnostics() end, { desc = "Toggle Diagnostics" })
+local conceallevel = vim.o.conceallevel > 0 and vim.o.conceallevel or 3
+map("n", "<leader>uc", function() Util.toggle("conceallevel", false, {0, conceallevel}) end, { desc = "Toggle Conceal" })
+if vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint then
+  map( "n", "<leader>uh", function() Util.toggle.inlay_hints() end, { desc = "Toggle Inlay Hints" })
+end
+map("n", "<leader>uT", function() if vim.b.ts_highlight then vim.treesitter.stop() else vim.treesitter.start() end end, { desc = "Toggle Treesitter Highlight" })
+map("n", "<leader>ub", function() Util.toggle("background", false, {"light", "dark"}) end, { desc = "Toggle Background" })
+
+-- lazygit
+map("n", "<leader>gg", function() Util.terminal({ "lazygit" }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false }) end, { desc = "Lazygit (root dir)" })
+map("n", "<leader>gG", function() Util.terminal({ "lazygit" }, {esc_esc = false, ctrl_hjkl = false}) end, { desc = "Lazygit (cwd)" })
+
 -- quit
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
 
 -- highlights under cursor
 map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
+
+-- floating terminal
+local lazyterm = function() Util.terminal(nil, { cwd = Util.root() }) end
+map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
+map("n", "<leader>fT", function() Util.terminal() end, { desc = "Terminal (cwd)" })
+map("n", "<c-/>", lazyterm, { desc = "Terminal (root dir)" })
+map("n", "<c-_>", lazyterm, { desc = "which_key_ignore" })
 
 -- Terminal Mappings
 map("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
--- lua/wrightbradley/config/options.lua
+++ lua/wrightbradley/config/options.lua
@@ -137,3 +137,20 @@ opt.foldlevel = 99
 
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
+
+if vim.fn.has("nvim-0.9.0") == 1 then
+  opt.statuscolumn = [[%!v:lua.require'wrightbradley.util'.ui.statuscolumn()]]
+  opt.foldtext = "v:lua.require'util'.ui.foldtext()"
+end
+
+-- HACK: causes freezes on <= 0.9, so only enable on >= 0.10 for now
+if vim.fn.has("nvim-0.10") == 1 then
+  opt.foldmethod = "expr"
+  opt.foldexpr = "v:lua.require'wrightbradley.util'.ui.foldexpr()"
+  opt.foldtext = ""
+  opt.fillchars = "fold: "
+else
+  opt.foldmethod = "indent"
+end
+
+opt.formatexpr = "v:lua.require'wrightbradley.util'.format.formatexpr()"
