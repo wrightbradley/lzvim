86a7e5b101ac75fce7fe8a728f84e95db693d22f
wrightbradley <bradley.wright.tech@gmail.com>
2024-04-15 08:22:33 -0400

    feat: configure plugins.linting.nvim-lint
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/linting/nvim-lint.lua
+++ lua/wrightbradley/plugins/linting/nvim-lint.lua
@@ -6,13 +6,13 @@ return {
       -- Event to trigger linters
       events = { "BufWritePost", "BufReadPost", "InsertLeave" },
       linters_by_ft = {
-        fish = { "fish" },
+        -- fish = { "fish" },
         -- Use the "*" filetype to run linters on all filetypes.
         -- ['*'] = { 'global linter' },
         -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
         -- ['_'] = { 'fallback linter' },
       },
-      -- LazyVim extension to easily override linter options
+      -- Util extension to easily override linter options
       -- or add custom linters.
       ---@type table<string,table>
       linters = {
@@ -27,8 +27,6 @@ return {
       },
     },
     config = function(_, opts)
-      -- --local Util = require("util")
-
       local M = {}
 
       local lint = require("lint")
