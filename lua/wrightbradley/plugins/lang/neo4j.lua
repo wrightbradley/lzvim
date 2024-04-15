aa5e4731ef124302aa8d78bc35191fce1b08ded4
wrightbradley <bradley.wright.tech@gmail.com>
2024-04-15 12:18:25 -0400

    feat: add cypher-language-support
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/neo4j.lua
+++ lua/wrightbradley/plugins/lang/neo4j.lua
@@ -2,4 +2,20 @@ return {
   {
     "neo4j-contrib/cypher-vim-syntax",
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        cypher_ls = {},
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "cypher-language-server",
+      })
+    end,
+  },
 }
