79cac2cb5934f7f781451b2e21231fb7106c1122
wrightbradley <bradley.wright.tech@gmail.com>
2024-04-15 08:31:43 -0400

    feat: configure plugins.core.ui.lualine
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/core/ui/lualine.lua
+++ lua/wrightbradley/plugins/core/ui/lualine.lua
@@ -1,5 +1,4 @@
 -- statusline
--- --local Util = require("util")
 return {
   {
     "nvim-lualine/lualine.nvim",
@@ -19,7 +18,7 @@ return {
       local lualine_require = require("lualine_require")
       lualine_require.require = require
 
-      -- local icons = require("config").icons
+      local icons = require("wrightbradley.config").icons
 
       vim.o.laststatus = vim.g.lualine_laststatus
 
@@ -34,50 +33,50 @@ return {
           lualine_b = { "branch" },
 
           lualine_c = {
-            -- Util.lualine.root_dir(),
-            -- {
-            --   "diagnostics",
-            --   symbols = {
-            --     error = icons.diagnostics.Error,
-            --     warn = icons.diagnostics.Warn,
-            --     info = icons.diagnostics.Info,
-            --     hint = icons.diagnostics.Hint,
-            --   },
-            -- },
+            Util.lualine.root_dir(),
+            {
+              "diagnostics",
+              symbols = {
+                error = icons.diagnostics.Error,
+                warn = icons.diagnostics.Warn,
+                info = icons.diagnostics.Info,
+                hint = icons.diagnostics.Hint,
+              },
+            },
             { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-            -- { Util.lualine.pretty_path() },
+            { Util.lualine.pretty_path() },
           },
           lualine_x = {
             -- stylua: ignore
             {
               function() return require("noice").api.status.command.get() end,
               cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-              -- color = Util.ui.fg("Statement"),
+              color = Util.ui.fg("Statement"),
             },
             -- stylua: ignore
             {
               function() return require("noice").api.status.mode.get() end,
               cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-              -- color = Util.ui.fg("Constant"),
+              color = Util.ui.fg("Constant"),
             },
             -- stylua: ignore
             {
               function() return "  " .. require("dap").status() end,
               cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
-              -- color = Util.ui.fg("Debug"),
+              color = Util.ui.fg("Debug"),
             },
             {
               require("lazy.status").updates,
               cond = require("lazy.status").has_updates,
-              -- color = Util.ui.fg("Special"),
+              color = Util.ui.fg("Special"),
             },
             {
               "diff",
-              -- symbols = {
-              --   added = icons.git.added,
-              --   modified = icons.git.modified,
-              --   removed = icons.git.removed,
-              -- },
+              symbols = {
+                added = icons.git.added,
+                modified = icons.git.modified,
+                removed = icons.git.removed,
+              },
               source = function()
                 local gitsigns = vim.b.gitsigns_status_dict
                 if gitsigns then
