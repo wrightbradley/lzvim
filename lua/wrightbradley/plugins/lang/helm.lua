ef439e1b145e16633608f376e4a3229848029bc6
wrightbradley <bradley.wright.tech@gmail.com>
2024-04-15 08:25:42 -0400

    feat: configure plugins.lang.helm
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/helm.lua
+++ lua/wrightbradley/plugins/lang/helm.lua
@@ -1,12 +1,12 @@
--- require("util").lsp.on_attach(function(client, buffer)
---   if client.name == "yamlls" then
---     if vim.api.nvim_get_option_value("filetype", { buf = buffer }) == "helm" then
---       vim.schedule(function()
---         vim.cmd("LspStop ++force yamlls")
---       end)
---     end
---   end
--- end)
+Util.lsp.on_attach(function(client, buffer)
+  if client.name == "yamlls" then
+    if vim.api.nvim_get_option_value("filetype", { buf = buffer }) == "helm" then
+      vim.schedule(function()
+        vim.cmd("LspStop ++force yamlls")
+      end)
+    end
+  end
+end)
 
 return {
   { "towolf/vim-helm", ft = "helm" },
