da95d3415b45133c4a759cb9b62ed5097197cfda
wrightbradley <bradley.wright.tech@gmail.com>
2024-04-15 08:30:31 -0400

    feat: configure plugins.lang.yaml
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/yaml.lua
+++ lua/wrightbradley/plugins/lang/yaml.lua
@@ -63,13 +63,13 @@ return {
       setup = {
         yamlls = function()
           -- Neovim < 0.10 does not have dynamic registration for formatting
-          -- if vim.fn.has("nvim-0.10") == 0 then
-          --   require("util").lsp.on_attach(function(client, _)
-          --     if client.name == "yamlls" then
-          --       client.server_capabilities.documentFormattingProvider = true
-          --     end
-          --   end)
-          -- end
+          if vim.fn.has("nvim-0.10") == 0 then
+            Util.lsp.on_attach(function(client, _)
+              if client.name == "yamlls" then
+                client.server_capabilities.documentFormattingProvider = true
+              end
+            end)
+          end
         end,
       },
     },
