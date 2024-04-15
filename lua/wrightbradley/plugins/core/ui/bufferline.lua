1645aacf62eab0860087256493cf6c076e7b1860
wrightbradley <bradley.wright.tech@gmail.com>
2024-04-15 09:46:13 -0400

    feat: configure plugins.core.ui.bufferline
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/core/ui/bufferline.lua
+++ lua/wrightbradley/plugins/core/ui/bufferline.lua
@@ -24,7 +24,7 @@ return {
         diagnostics = "nvim_lsp",
         always_show_bufferline = false,
         diagnostics_indicator = function(_, _, diag)
-          local icons = require("config").icons.diagnostics
+          local icons = require("wrightbradley.config").icons.diagnostics
           local ret = (diag.error and icons.Error .. diag.error .. " " or "")
             .. (diag.warning and icons.Warn .. diag.warning or "")
           return vim.trim(ret)
