b0251cfffb20b7fa6a5da5429b8ef70cb65f01d6
wrightbradley <bradley.wright.tech@gmail.com>
2024-04-15 12:13:45 -0400

    feat: add bash support
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

---
+++ lua/wrightbradley/plugins/lang/bash.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "bash" })
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        bashls = {},
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "bash-language-server",
+        "shfmt",
+        "shellcheck",
+      })
+    end,
+  },
+}
