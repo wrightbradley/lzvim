1b6174af47cb09f443356350194c9ac22a4a1566
wrightbradley <bradley.wright.tech@gmail.com>
2024-04-15 07:52:21 -0400

    feat: configure util.lazygit
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/util/lazygit.lua
+++ lua/wrightbradley/util/lazygit.lua
@@ -1,6 +1,4 @@
---local Util = require("util")
-
----@class util.lazygit
+---@class wrightbradley.util.lazygit
 ---@field config_dir? string
 ---@overload fun(cmd: string|string[], opts: LazyTermOpts): LazyFloat
 local M = setmetatable({}, {
@@ -140,6 +138,24 @@ gui:
   config = config .. table.concat(lines, "\n")
   require("lazy.util").write_file(M.theme_path, config)
   M.dirty = false
+end
+
+---@param opts? {count?: number}|LazyCmdOptions
+function M.blame_line(opts)
+  opts = vim.tbl_deep_extend("force", {
+    count = 3,
+    filetype = "git",
+    size = {
+      width = 0.6,
+      height = 0.6,
+    },
+    border = "rounded",
+  }, opts or {})
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local line = cursor[1] - 1
+  local file = vim.api.nvim_buf_get_name(0)
+  local cmd = { "git", "log", "-n", opts.count, "-u", "-L", line .. ",+1:" .. file }
+  return require("lazy.util").float_cmd(cmd, opts)
 end
 
 return M
