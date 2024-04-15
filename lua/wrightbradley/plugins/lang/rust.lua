90f83c9e92ac8a25b9fb6ed7e013cb6289c39d65
wrightbradley <bradley.wright.tech@gmail.com>
2024-04-15 08:38:42 -0400

    feat: configure plugins.lang.rust
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/rust.lua
+++ lua/wrightbradley/plugins/lang/rust.lua
@@ -1,5 +1,4 @@
 return {
-
   -- Extend auto completion
   {
     "hrsh7th/nvim-cmp",
@@ -137,7 +136,7 @@ return {
       },
       setup = {
         rust_analyzer = function(_, opts)
-          -- local rust_tools_opts = require("util").opts("rust-tools.nvim")
+          local rust_tools_opts = Util.opts("rust-tools.nvim")
           require("rust-tools").setup(vim.tbl_deep_extend("force", rust_tools_opts or {}, { server = opts }))
           return true
         end,
