558e48e85b4564bcbac8a4a32f2a92524ba7f01e
wrightbradley <bradley.wright.tech@gmail.com>
2024-04-15 12:16:44 -0400

    feat: add yamllint and yamlfmt support
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- lua/wrightbradley/plugins/lang/yaml.lua
+++ lua/wrightbradley/plugins/lang/yaml.lua
@@ -20,6 +20,15 @@ return {
   -- correctly setup lspconfig
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "yamlfmt", "yamllint" })
+        end,
+      },
+    },
     opts = {
       -- make sure mason installs the server
       servers = {
